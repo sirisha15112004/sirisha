16d050de0e3c930b626038d63fce5ed55f307159
sirisha15112004 <161101839+sirisha15112004@users.noreply.github.com>
2024-03-24 20:42:07 +0530

    Add files via upload

---
+++ eligible for vote sim.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n=16,a;
+	if(a>=18)
+	{
+		printf("eligible for vote");
+	}
+	else
+	{
+		printf("not eligible for vote\nu will be eligible after %d years",18-a);
+}
+return 0;
+			
+}
